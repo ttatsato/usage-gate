6f99e6d3b29fd25d4ab6f277b82eb98737a46324
ttatsato <t-satoh@alby.co.jp>
2026-04-13 19:24:56 +0900

    add: add create sqlx

--- README.dev.md
+++ README.dev.md
@@ -9,3 +9,28 @@ cp .env.example .env
 ```
 docker compose up -d
 ```
+
+# Run API Server
+
+```
+cargo run
+```
+
+
+# TIPS
+
+## Create migration file
+```
+sqlx migrate add <MIGRATION_NAME>
+```
+
+### Execute migration
+```
+sqlx migrate run
+```
+
+### Check Out If Created Table
+```
+docker exec -it usage-gate-db psql -U usage_gate -d usage_gate -c '\dt'
+docker exec -it usage-gate-db psql -U usage_gate -d usage_gate -c '\d tenants'
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 use axum::{Json, routing::get, Router};
 use serde::Serialize;
+use sqlx::postgres::PgPoolOptions;
 
 
 #[derive(Serialize)]
@@ -16,7 +17,15 @@ async fn health () -> Json<HealthResponse> {
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    dotenv::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
+    let pool = PgPoolOptions::new()
+        // NOTE: プール数目安 = CPU コア数 × 2 + 1
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to database");
+
     // with_env_filterは出力するログステータスを制御
     // NOTE: RUST_LOGという環境変数で設定可能
     tracing_subscriber::fmt().with_env_filter(
@@ -24,7 +33,7 @@ async fn main() {
     )
     .init();
 
-    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let port = std::env::var("API_PORT").unwrap_or_else(|_| "8080".to_string());
     let app = Router::new().route("/health", get(health));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:".to_string() + &port).await.expect("Failed to bind port");
